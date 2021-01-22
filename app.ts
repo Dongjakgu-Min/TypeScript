7ad1f682f647a54e65fb5f2756bc0f02b2fbe277
Song <songyw9812@gmail.com>
2021-01-22 17:56:50 +0900

    Initial Commit

---
+++ app.ts
@@ -0,0 +1,38 @@
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import indexRouter from './router/index';
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+
+console.log(__dirname);
+
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.use(function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  next(createError(404));
+})
+
+app.use(function (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+export default app;
